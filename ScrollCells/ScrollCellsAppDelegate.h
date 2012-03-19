f0cfe1ce309332dc7b1291b1644a92a61c72b9e6
Douglas Hill <Douglas2967243891@googlemail.com>
2012-03-19 21:22:01 +0000

    Initial Commit

---
+++ ScrollCells/ScrollCellsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ScrollCellsAppDelegate.h
+//  ScrollCells
+//
+//  Created by Douglas Hill on 19/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollCellsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollCells/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScrollCells
+//
+//  Created by Douglas Hill on 19/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ScrollCellsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ScrollCellsAppDelegate class]));
+	}
+}
